e9f277cda53bedca66fbebcf587e1c9d81279e02
andyfoster <andrewfosteris@gmail.com>
2015-09-15 18:00:45 +1200

    Added status messages

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 var http = require("http");
 
 http.createServer(function(request, response){
+  console.log("Request received" + Date.now());
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
 }).listen(8888);
+// if you run
+// node server.js and then go to:
+// http://localhost:8888/
+// you will see your web server up and running
+
+console.log("Server has started");
+
+// We could refactor to:
+//
+// var http = require("http");
+//
+// function onRequest(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }
+//
+// http.createServer(onRequest).listen(8888);
