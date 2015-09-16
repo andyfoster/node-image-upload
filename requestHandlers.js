b3e5dd7460e3cbd1e656ec36d3ddb2a75c082176
andyfoster <andrewfosteris@gmail.com>
2015-09-16 17:13:36 +1200

    Testing returning text from the  requestHandler functions

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,10 @@
 function start() {
-  console.log("Request handler 'start' was called");
+  console.log("Request handler 'START' was called");
+  return "Hello Start";
 }
 function upload() {
-  console.log("Request handler 'upload' was called");
+  console.log("Request handler 'UPLOAD' was called");
+  return "Hello Upload";
 }
 
 exports.start = start;
