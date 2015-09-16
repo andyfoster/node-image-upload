117c323b46451a97c08989e6ee52607deb1aefa3
andyfoster <andrewfosteris@gmail.com>
2015-09-16 17:46:43 +1200

    Demonstrate a blocking call in the middle

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,16 @@
 function start() {
   console.log("Request handler 'START' was called");
+
+  function sleep(milliSeconds) {
+    // simulate a slow blocking process like grabbing an API etc
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+  sleep(5000);
+  // this actually blocks up the upload page as well if it is called
+  // at the same time
   return "Hello Start";
+
 }
 function upload() {
   console.log("Request handler 'UPLOAD' was called");
