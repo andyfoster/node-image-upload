aa156c2233a3a8379ff35745d5cb7a8c0a2b86f9
andyfoster <andrewfosteris@gmail.com>
2015-09-16 18:35:18 +1200

    Displaying folder contents in stdout

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,20 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
   console.log("Request handler 'START' was called");
 
-  function sleep(milliSeconds) {
-    // simulate a slow blocking process like grabbing an API etc
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
-  sleep(5000);
-  // this actually blocks up the upload page as well if it is called
-  // at the same time
-  return "Hello Start";
-
+  exec("ls -lah", function(error, stdout, stderr){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
+  });
 }
-function upload() {
+
+function upload(response) {
   console.log("Request handler 'UPLOAD' was called");
-  return "Hello Upload";
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -7,13 +7,8 @@ function start(route, handle){
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname +" received");
 
-    route(handle, pathname);
+    route(handle, pathname, response);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var content = route(handle, pathname);
-    // response.write("Hello World");
-    response.write(content);
-    response.end();
   }
 
   http.createServer(onRequest).listen(8888);
